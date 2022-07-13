a7b06a904c5b3609609ad4159a323c1efd5ad2d5
WillOli <william.ika@gmail.com>
2022-07-13 17:40:22 -0300

    Porject Clean

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+/*Esta é a estrutura básica de uma aplicação React
+Ele é responsável por processar as informações que está no projeto e apresentar o resultado(renderização.)*/
